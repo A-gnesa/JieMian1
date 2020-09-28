799b82af6ab8adbb173c85aa4e59398fd305a2d1
chengzihan <1908932404@qq.com>
2020-09-28 20:41:34 +0800

    差底部按键切换  groupradio实现了 这个版本按底部的按钮不好用

--- app/src/main/java/com/example/jiemian/MainActivity.java
+++ app/src/main/java/com/example/jiemian/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.jiemian;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -18,23 +22,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private Context context;
     private List<Integer> list;
+
     private RadioGroup radioGroup;
+    /*FragmentManager要管理fragment（添加，替换以及其他的执行动作）
+     *的一系列的事务变化，需要通过fragmentTransaction来操作执行
+     */
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    View message;
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
+        //用LayoutInflater加载布局
+        LayoutInflater fragment_message = LayoutInflater.from(context);
+        //获取message所在的View
+        message = fragment_message.inflate(R.layout.fragment_radiobutton,null);
+        //找到radioGroup
+        radioGroup = message.findViewById(R.id.RadioGroup);
         //初始化fragmentManager
         fragmentManager = getSupportFragmentManager();
         //初始化Recycler
         init_Recycler();
+        init_first_fragment();
+        //初始化RadioGroup 实现切换Fragment
 
+        init_RadioGroup();
     }
     void init_Recycler(){
         //赋值
@@ -59,24 +78,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
     void init_RadioGroup(){
-        //初始化RadioGroup 实现切换Fragment
-        radioGroup = findViewById(R.id.RadioGroup);
-        //定义点击事件
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        RadioButton button_message = message.findViewById(R.id.message);
+        button_message.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.message :
-                        /*FragmentManager要管理fragment（添加，替换以及其他的执行动作）
-                         *的一系列的事务变化，需要通过fragmentTransaction来操作执行
-                         */
-                        fragmentTransaction = fragmentManager.beginTransaction();
-                        fragment_Message message_fragment = new fragment_Message();
-
-                        break;
-                    case R.id.Mine:
-                        break;
-                }
+            public void onClick(View v) {
+                Toast.makeText(context,"A",Toast.LENGTH_LONG);
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragment_Message message_fragment = new fragment_Message();
+                fragmentTransaction.add(R.id.RelativeLayout,message_fragment);
+                fragmentTransaction.commit();
             }
         });
     }
